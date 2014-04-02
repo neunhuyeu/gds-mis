4811ccdfbc8e6a019568409f8d1c9348d0b50f8f
Joshua <joshuapalm00@gmail.com>
2014-04-02 23:08:34 +0200

    Added addPatient method to dbAccessLayer and CSynchronise.
    Also added smoking frequency to patient.

--- DMS Service/DbAccessLayer.cs
+++ DMS Service/DbAccessLayer.cs
@@ -56,6 +56,68 @@ namespace DMS_Service
             return dbConnection.SelectQuery(query, sqlParameters);
         }
 
+        /// <summary>
+        /// JP.
+        /// Adds patient to the database by first adding record to person table and then patient table.
+        /// </summary>
+        /// <param name="patient">patient to be added</param>
+        public void addPatient(Patient patient)
+        {
+            //query for person table
+            string query = "INSERT INTO person(first_name, last_name, date_of_birth, email_address, mobile_number, landline_number, home_addres) VALUES ('@first_name', '@last_name', @date_of_birth, '@email_address', '@mobile_number', '@landline_number', '@home_address')";
+
+            //parameters for person table
+            SqlParameter[] sqlParameters = new SqlParameter[7];
+            sqlParameters[0] = new SqlParameter("@first_name", SqlDbType.Char);
+            sqlParameters[0].Value = Convert.ToString(patient.FirstName);
+            sqlParameters[1] = new SqlParameter("@last_name", SqlDbType.Char);
+            sqlParameters[1].Value = Convert.ToString(patient.LastName);
+            sqlParameters[2] = new SqlParameter("@date_of_birth", SqlDbType.Date);
+            sqlParameters[2].Value = Convert.ToString(patient.DateOfBirth);
+            sqlParameters[3] = new SqlParameter("@email_address", SqlDbType.Char);
+            sqlParameters[3].Value = Convert.ToString(patient.Email);
+            sqlParameters[4] = new SqlParameter("@mobile_number", SqlDbType.Char);
+            sqlParameters[4].Value = Convert.ToString(patient.MobileNumber);
+            sqlParameters[5] = new SqlParameter("@landline_number", SqlDbType.Char);
+            sqlParameters[5].Value = Convert.ToString(patient.LandLineNumber);
+            sqlParameters[6] = new SqlParameter("@home_address", SqlDbType.Char);
+            sqlParameters[6].Value = Convert.ToString(patient.Address);
+
+            //execute query on person table
+            dbConnection.InsertQuery(query, sqlParameters);
+
+            //query for patient table
+            query = "INSERT INTO patients (gender, height_cm, weight_kg, blood_type, smoking, smoking_frequency, hard_drugs, hard_drugs_frequency, person_id) VALUES ('@gender', @height_cm, @weight_kg, '@blood_type', @smoking Bool, @smoking_frequency, @hard_drugs, @hard_drugs_frequency, (SELECT MAX(person_ID) FROM person))";
+
+
+            //parameters for patient table
+            sqlParameters = null;
+            sqlParameters = new SqlParameter[9];
+            sqlParameters[0] = new SqlParameter("@gender", SqlDbType.Char);
+            sqlParameters[0].Value = Convert.ToString(patient.Gender);
+            sqlParameters[1] = new SqlParameter("@height_cm", SqlDbType.Int);
+            sqlParameters[1].Value = Convert.ToString(patient.Height);
+            sqlParameters[2] = new SqlParameter("@weight_kg", SqlDbType.Int);
+            sqlParameters[2].Value = Convert.ToString(patient.Weight);
+            sqlParameters[3] = new SqlParameter("@blood_type", SqlDbType.Char);
+            sqlParameters[3].Value = Convert.ToString(patient.BloodType);
+            sqlParameters[4] = new SqlParameter("@smoking", SqlDbType.Bit);
+            sqlParameters[4].Value = Convert.ToString(patient.Smoker);
+            sqlParameters[5] = new SqlParameter("@smoking_frequency", SqlDbType.Int);
+            sqlParameters[5].Value = Convert.ToString(patient.SmokingFrequency);
+
+            //TODO: Add correct paameters
+            sqlParameters[6] = new SqlParameter("@hard_drugs", SqlDbType.Bit);
+            sqlParameters[6].Value = false;
+            sqlParameters[7] = new SqlParameter("@smoking_frequency", SqlDbType.Int);
+            sqlParameters[7].Value = 0;
+
+            //execute query on patient table
+            dbConnection.InsertQuery(query, sqlParameters);
+
+            return;
+        }
+
         
     }
 }
