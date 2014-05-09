40fb3b7bf5f7a0a0ce3ecdf927d97a1979da4984
Torben Zurhelle <ferusolin@hotmail.de>
2014-05-09 11:07:21 +0200

    Cleaning Up the structure  Solution explorer

---
+++ Appointment Serves/user_authentication/user_auth_access.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using DMS_Service.database_connection;
+
+namespace DMS_Service.user_auth
+{
+    class user_auth_access
+    {
+        private db_connection db_con;
+
+        public user_auth_access()
+        {
+            db_con = new db_connection("gds_mis_auth");
+        }
+
+        public DataTable user_login(string email, string passw)
+        {
+            string query = string.Format("SELECT email_address, password " +
+                                         "From user_auth " + 
+                                         "WHERE email_address = @email " +
+                                         "AND password = @passw ");
+
+            MySqlParameter[] query_parameters = new MySqlParameter[2];
+            query_parameters[0] = new MySqlParameter("@passw", MySqlDbType.VarChar);
+            query_parameters[0].Value = Convert.ToString(passw);
+            query_parameters[1] = new MySqlParameter("@email", MySqlDbType.VarChar);
+            query_parameters[1].Value = Convert.ToString(email);
+            return db_con.SelectQuery(query, query_parameters);
+        }
+    }
+}
---
+++ Appointment Serves/user_authentication/user_auth_business.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Windows.Forms;
+using DMS_Service;
+
+
+namespace DMS_Service.user_auth
+{
+    class user_auth_business
+    {
+        private user_auth_access db_access;
+
+        public user_auth_business()
+        {
+            db_access = new user_auth_access();
+        }
+
+        public bool login(string email, string passw)
+        {
+            DataTable temp_datatable = new DataTable();
+            temp_datatable = db_access.user_login(email, passw);
+            string temp_mail = "";
+            string temp_passw = "";
+            if (temp_datatable != null)
+            {
+                foreach (DataRow row in temp_datatable.Rows)
+                {
+                    temp_mail = row["email_address"].ToString();
+                    temp_passw = row["password"].ToString();    
+                }
+                if ((temp_mail == email) && (temp_passw == passw))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
+}
